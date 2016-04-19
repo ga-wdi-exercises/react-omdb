702afc86e8b88f47960370825158d1d0863cb062
Nick Olds <nolds9@yahoo.com>
2016-04-18 21:25:27 -0400

    adds Home Component

---
+++ app/components/Home.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Home = React.createClass({
+  render: function () {
+    return (
+      <div className={"jumbotron col-sm-12 text-center"}>
+        <h1>React Omdb</h1>
+      </div>
+    )
+  }
+});
+
+module.exports = Home;
---
+++ app/index.js
@@ -0,0 +1,9 @@
+var React = require("react");
+var ReactDom = require("react-dom");
+
+var Home = require("./components/Home");
+
+ReactDom.render(
+  <Home />,
+  document.getElementById("app")
+);
