2223c7dc7de6fa3de7aab931b2b982840f5f4ea1
Nick Olds <nolds9@yahoo.com>
2016-04-18 21:29:55 -0400

    adds transparent background styles

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,9 +1,10 @@
 var React = require('react');
+var styles = require("../styles/index");
 
 var Home = React.createClass({
   render: function () {
     return (
-      <div className={"jumbotron col-sm-12 text-center"}>
+      <div className={"jumbotron col-sm-12 text-center"} style={styles.transparentBg}>
         <h1>React Omdb</h1>
       </div>
     )
