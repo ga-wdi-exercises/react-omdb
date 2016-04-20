82ed3d3eec648efe638ac6150babe908f24dcc31
Nick Olds <nolds9@yahoo.com>
2016-04-19 23:31:22 -0400

    installs dependencies, configures build, and jello world

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebPackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + "/app/index.html",
+  filename: "index.html",
+  inject: 'body'
+});
+
+module.exports = {
+  // What file to feed into webpack
+  entry: [
+    "./app/index.js"
+  ],
+  // Where we want our outputted files to go
+  output: {
+    path: __dirname + '/dist',
+    filename: "index_bundle.js"
+  },
+  // What transformations we want to apply to our code
+  module: {
+    loaders: [
+      {
+        test: /\.js$/, // target any file ending in js
+        exclude: /node_modules/, // except for node_modules
+        loader: 'babel-loader' // apply the babel loader
+      }
+    ]
+  },
+  plugins: [
+   HtmlWebPackPluginConfig
+]
+}
