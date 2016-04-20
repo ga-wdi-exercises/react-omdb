f1f4c044ea56754b2b43cc5699a260f221178171
Nick Olds <nolds9@yahoo.com>
2016-04-19 23:38:59 -0400

    adds Hello world React

---
+++ app/components/HelloWorld.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var HelloWorld = React.createClass({
+  render: function () {
+    return (
+      <div>Jello World!</div>
+    )
+  }
+});
+
+module.exports = HelloWorld;
--- app/index.js
+++ app/index.js
@@ -1,1 +1,12 @@
-document.getElementById("app").innerHTML = "Jello World";
+// Bring in React and ReactDom
+var React = require('react');
+var ReactDOM = require("react-dom");
+
+// Load HelloWorld Component
+var HelloWorld = require("./components/HelloWorld");
+
+// Render HelloWorld
+ReactDOM.render(
+  <HelloWorld />, // our component
+  document.getElementById("app") // where to render
+)
