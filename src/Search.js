8d4d671801909adfdf871c12dbfb6daff28d242c
Nick Olds <nolds9@yahoo.com>
2016-08-02 15:24:57 -0400

    4: Adds Search Container

--- src/Search.js
+++ src/Search.js
@@ -1,30 +1,14 @@
 import React, {Component} from "react"
 
 class Search extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      query: ''
-    }
-  }
-  handleSearchInput (e) {
-    this.setState({
-      query: e.target.value
-    })
-  }
-
-  submitQuery(evt){
-    evt.preventDefault();
-    console.log(this.state.query)
-  }
-
   render(){
+    let {handleSearchInput, handleSubmitQuery, query} = this.props
     return (
       <div className="searchBox">
-        <form onSubmit={(evt) => this.submitQuery(evt)}>
+        <form onSubmit={(evt) => handleSubmitQuery(evt)}>
           <input
-          onChange={(evt) => this.handleSearchInput(evt)}
-          value={this.state.query}
+          onChange={(evt) => handleSearchInput(evt)}
+          value={query}
           type="text"
           placeholder="Enter a Movie Title..." />
           <button type="submit">Search</button>
---
+++ src/SearchContainer.js
@@ -0,0 +1,37 @@
+import React, {Component} from "react"
+import Search from "./Search"
+
+class SearchContainer extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      query: ''
+    }
+  }
+
+  onSearchInput (evt) {
+    this.setState({
+      query: evt.target.value
+    })
+  }
+
+  onSubmitQuery(evt){
+    evt.preventDefault();
+    console.log(this.state.query)
+    this.setState({
+      query: ''
+    })
+  }
+
+  render(){
+    return (
+      <Search
+        handleSearchInput={ (evt) => this.onSearchInput(evt) }
+        handleSubmitQuery={ (evt) => this.onSubmitQuery(evt) }
+        query={this.state.query}
+      />
+    )
+  }
+}
+
+export default SearchContainer
