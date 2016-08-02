da936065246dc91a5be826602b3550d05b0adffd
Nick Olds <nolds9@yahoo.com>
2016-08-02 15:43:03 -0400

    5: Renders hard-coded results

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import SearchContainer from "./SearchContainer"
 
+
 class Home extends Component {
   render() {
     return (
       <div>
-        <h1>OMDB React</h1>
+        <h1><a href="/">OMDB React</a></h1>
         <SearchContainer />
       </div>
     );
