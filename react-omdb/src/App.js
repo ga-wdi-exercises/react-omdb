cc50109f5519abbdffca4a73857743f5e43711da
Adrian Maseda <amaseda@gmail.com>
2016-08-02 14:15:44 -0400

    1: HelloWorld component

---
+++ react-omdb/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class HelloWorld extends Component {
+  render() {
+    return (
+      <div>Hello world.</div>
+    );
+  }
+}
+
+export default HelloWorld;
