0024bef69f88dd9a5e086762a151b6063ecdf511
Nick Olds <nolds9@yahoo.com>
2016-08-02 14:50:52 -0400

    1: Set up a HelloWorld Component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import HelloWorld from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <HelloWorld />,
   document.getElementById('root')
 );
