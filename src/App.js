9ddcda2e1df286d9911774e54783152858f04885
Poh Chuan Kuan <pohckuan@yahoo.com>
2016-11-15 15:36:22 -0500

    hello world

--- src/App.js
+++ src/App.js
@@ -1,1 +1,14 @@
 // Developer TODO: Define App component defintion here
+
+import React, {Component} from 'react'
+
+// const HelloWorld = props => <h1> Hello World </h1>
+class HelloWorld extends Component {
+  render() {
+    return (
+      <h1> Hello World </h1>
+    )
+  }
+}
+
+export default HelloWorld
--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 // Developer TODO: Load in App component and render to the DOM
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import HelloWorld from './App.js'
+
+
+ReactDOM.render(
+  <HelloWorld />,
+  document.getElementById('root')
+)
