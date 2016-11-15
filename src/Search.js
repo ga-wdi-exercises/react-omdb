a50f93b78c9af2669db15b2b42fea92ec96f0726
Tim Foley <thetimfoley@gmail.com>
2016-11-15 15:48:45 -0500

    Add search button

---
+++ src/Search.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  render() {
+    return (
+      <div className="search">
+        <form>
+          <input type="text"></input>
+          <button type="submit">SEARCH</button>
+        </form>
+
+      </div>
+    )
+  }
+}
+
+export default Search
