a26dee11af315d71c8150846f7d8336d5b1f78b3
Nick Oki <nick.oki@live.com>
2016-11-15 15:34:12 -0500

    Steps 1 and 2

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+// Developer TODO: Define App component defintion here
+import React, { Component } from 'react'
+
+class Home extends Component {
+  render() {
+    return(
+      <h1>Welcome</h1>
+    )
+  }
+}
+
+export default Home
