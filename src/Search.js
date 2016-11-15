2850a8dbdb131d83b5c41fb8dc0ffeee7efab935
Nick Oki <nick.oki@live.com>
2016-11-15 16:23:52 -0500

    Add search logic

--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,29 @@
 import React, { Component} from 'react'
 
 class Search extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      searchTerm: ''
+    }
+  }
+
+  handleSearchInput(e) {
+    this.setState({
+      searchTerm: e.target.value
+    })
+  }
+
+  handleSearchSubmit(e) {
+    e.preventDefault()
+    console.log(this.state.searchTerm)
+  }
+
   render() {
     return(
       <div className="search">
-        <form>
-          <input type="text" placeholder="Enter a movie title" />
+        <form onSubmit={e => this.handleSearchSubmit(e)}>
+          <input type="text" placeholder="Enter a movie title" onChange={ e => this.handleSearchInput(e)} value={this.state.searchTerm} />
           <button type="submit">Submit</button>
         </form>
       </div>
