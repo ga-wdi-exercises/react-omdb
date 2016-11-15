eb1c59255f37222c7def0929a6d8afbafcc90bd0
Michael Rubin <michael@rubinsworld.com>
2016-11-15 13:26:58 -0500

    Step 1 completed. Hello World working

--- src/App.js
+++ src/App.js
@@ -1,1 +1,16 @@
 // Developer TODO: Define App component defintion here
+// bring in React and Component instance from react
+import React, {Component} from 'react'
+
+// define our Hello component
+class Hello extends Component {
+  // what should the component render
+  render () {
+    // Make sure to return some UI
+    return (
+      <h1>Hello World!</h1>
+    )
+  }
+}
+
+export default Hello
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 // Developer TODO: Load in App component and render to the DOM
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Hello from './App.js'
+
+ReactDOM.render(
+  <Hello />,
+  document.getElementById('root')
+)
