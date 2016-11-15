80c7d4f5a43c20868fe8f6787371cc1ee260ad93
Domonique Griffin <griffind2009@gmail.com>
2016-11-15 15:11:06 -0500

    saving changes

--- src/Home.js
+++ src/Home.js
@@ -6,6 +6,7 @@ class Home extends Component {
     return (
       <div className="container">
       <h1></h1>
+      <Search />
       </div>
     )
   }
--- src/Search.js
+++ src/Search.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
+  constructor(props) {
+    super()
+    this.state = {
+      query: ''
+    }
+  }
+  handleChange(e){
+    this.setState({
+      query: this.state.query
+      // event.target.query
+    })
+  }
+    handleSubmit(e) {
+      console.log("clicked")
+        event.preventDefault()
+    }
+
   render() {
     return (
       <h1>Search</h1>
-    <form onSubmit={this.handleSubmit}>
-    <input type="text" value={this.state.value} onChange={this.handleChange} />
-    <input type="submit" value="Submit" />
-    </form>
+      <div>
+      <form onSubmit={this.handleSubmit}>
+      <input type="text" value={this.state.query} onChange={this.handleChange} />
+      <input type="submit" value="Submit" />
+      </form>
+      </div>
     )
   }
 }
