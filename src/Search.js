0b1c05c46d487fdfd23a7bdbca82b269c5c9b157
Tim Foley <thetimfoley@gmail.com>
2016-11-15 16:12:02 -0500

    Start wiring up search

--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      query: "",
+    }
+  }
+
+  handleSearchSubmit(e) {
+    e.preventDefault()
+    console.log(e);
+  }
+
+  updateQuery(e) {
+    this.setState({
+      query: e.target.value
+    })
+  }
+
+
   render() {
     return (
       <div className="search">
-        <form>
-          <input type="text"></input>
+        <form onSubmit={e => this.handleSearchSubmit(e)}>
+          <input onChange={e => this.updateQuery(e) } type="text" placeholder="Enter a movie title..." ></input>
           <button type="submit">SEARCH</button>
         </form>
 
