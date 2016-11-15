f4f356a0cf3234aa28f2e24f840eeaea1c3e5c1f
William Keating <keating49@gmail.com>
2016-11-15 15:45:44 -0500

    state to track search query

--- src/Search.js
+++ src/Search.js
@@ -1,10 +1,14 @@
 import React, {Component} from "react"
 
 class Search extends Component {
+  submitQuery(evt){
+    evt.preventDefault();
+    console.log("submit!")
+  }
   render() {
     return(
       <div className="searchBox">
-        <form>
+        <form onSubmit={(evt) => this.submitQuery(evt)}>
           <input type="text" placeholder="Enter a movie title..."/>
           <button type="submit">Search</button>
         </form>
