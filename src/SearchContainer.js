cae5f47ce0cad1f13c38460930cb63c784b12e77
William Keating <keating49@gmail.com>
2016-11-16 15:07:22 -0500

    connected to API

--- src/SearchContainer.js
+++ src/SearchContainer.js
@@ -1,42 +1,44 @@
 import React, {Component} from "react"
 import Search from "./Search"
 import Results from "./Results"
+import {queryOmdb} from "./Utils"
 
-class SearchContainer extends Component{
+class SearchContainer extends Component {
   constructor(props){
     super(props)
     this.state = {
       query: '',
       hasSearched: false,
+      movies: [],
     }
   }
-  onSearchInput(evt) {
+
+  onSearchInput (evt) {
     this.setState({
       query: evt.target.value,
     })
   }
 
   onSubmitQuery(evt){
-    evt.preventDefault();
-    console.log(this.state.query);
-    this.setState({
-      query: '',
-      hasSearched: true,
+    evt.preventDefault()
+    let component = this
+    queryOmdb(this.state.query).then( data => {
+      component.setState({
+        query: '',
+        hasSearched: true,
+        movies: data,
+      })
     })
   }
 
   render(){
-    let movies = [
-      {title: "Star Wars", poster_url: "http://fallmeeting.agu.org/2015/files/2015/12/Star-Wars.jpg"},
-      {title: "Top Gun", poster_url: "http://ecx.images-amazon.com/images/I/51YimkRDEjL._SY445_.jpg"}
-    ]
-    if(this.state.hasSearched){
+    if (this.state.hasSearched){
       return (
-        <Results movies={movies} />
+        <Results movies={this.state.movies} />
       )
     } else {
-      return (
-        <Search
+      return  (
+          <Search
           handleSearchInput={ (evt) => this.onSearchInput(evt) }
           handleSubmitQuery={ (evt) => this.onSubmitQuery(evt) }
           query={this.state.query} />
@@ -44,4 +46,5 @@ class SearchContainer extends Component{
     }
   }
 }
+
 export default SearchContainer
