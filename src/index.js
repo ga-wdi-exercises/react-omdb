b4472fb97651f3e780f91401f2daef2618f775ab
Alex Manno <alman92@gmail.com>
2016-11-17 08:51:55 -0500

    app.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 // Developer TODO: Load in App component and render to the DOM
+import React from 'react'
+
+
+const Search = props => {
+  return (
+    <form onSubmit={ e => props.onSearchSubmit(e) } >
+      <input
+        type="text"
+        value={props.searchTerm}
+        onChange={ e => props.onSearchInput(e) }
+        placeholder="Enter a Movie Title.." />
+      <input type="submit" />
+    </form>
+  )
+}
+
+
+export default Search
