a3062c406c9c1e5308493a1936d784b28dd55ca0
Erica Norby <ericanorby@gmail.com>
2017-03-27 15:36:00 -0400

    created home component

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+// Developer TODO: Define App component defintion here
+import React, { Component } from 'react'
+
+class Home extends Component {
+  render(){
+    return(
+      <div>
+        <h1>OMFG</h1>
+        <h3>Online Movie Finder Galaxy</h3>
+      </div>
+    )
+  }
+}
+
+export default Home
