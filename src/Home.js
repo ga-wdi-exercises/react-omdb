7b5e79fccd3f45727551a8d7957aee2c4ffc8a08
Collin OConnell <ococncol@gmail.com>
2017-03-27 17:00:27 -0400

    can display hard coded data but it displays all

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ class Home extends Component {
   render() {
     return(
       <div>
-      <h1>MooVee Findr</h1>
+      <h1><a href="/">MooVee Findr</a></h1>
       <SearchContainer />
       </div>
     )
