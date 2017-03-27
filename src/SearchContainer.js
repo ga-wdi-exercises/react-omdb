8f45ea3348e0fc2916ddf29101ab0d71eaafbd33
Eric Christopher Green <VERDE611@GMAIL.COM>
2017-03-27 16:39:26 -0400

    finished search container

--- src/SearchContainer.js
+++ src/SearchContainer.js
@@ -1,6 +1,35 @@
 import React, { Component } from 'react'
+import Search from './Search'
 
 class SearchContainer extends Component {
+  constructor(props){
+    super(props){
+      query: '',
+      movies: []
+    }
+  }
 
-  
+  onSearchInput(event){
+    this.setState({
+      query: event.target.value
+    })
+  }
+
+  onSubmitQuery(event){
+    event.preventDefault();
+    console.log(this.state.query)
+    this.setState({
+      query: ''
+    })
+  }
+
+  render(){
+    <Search
+      onSearchInput={(event) => this.onSearchInput(event)}
+      onSubmitQuery={(event) => this.onSubmitQuery(event)}
+    />
+  }
 }
+
+
+export default SearchContainer;
