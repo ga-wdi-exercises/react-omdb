da247b55e4bc0cbd376bd35144e953d5f4c16d7d
Collin OConnell <ococncol@gmail.com>
2017-03-27 15:33:03 -0400

    added home UI

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+  render() {
+    return(
+      <div>
+      <h1>MooVee Findr</h1>
+      </div>
+    )
+  }
+}
+export default Home
--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+/*jshint esversion:6*/
 // Developer TODO: Load in App component and render to the DOM
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Home from './Home'
+import './index.css'
+
+ReactDOM.render(
+  <Home className="container" />,
+  document.getElementById('root')
+)
