5bff73638086b81125b08a3f47196cdddeb33aa0
Erica Norby <ericanorby@gmail.com>
2017-03-28 19:54:03 -0400

    minor changes

--- src/SearchContainer.js
+++ src/SearchContainer.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import Search from './Search'
+import Results from './Results'
 
 
 class SearchContainer extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      query: ""
+      query: "",
+      hasSearched: false
     }
   }
 
@@ -19,16 +21,31 @@ class SearchContainer extends Component {
   onSubmitQuery(event) {
     event.preventDefault()
     console.log(this.state.query)
+    this.setState({
+      query: "",
+      hasSearched: true
+    })
   }
 
   render(){
-    return(
-      <Search
-        submitQuery={(e) => this.onSubmitQuery(e)}
-        searchInput={(e) => this.onSearchInput(e)}
-        query={this.state.query}
-      />
-    )
+    let movies = [
+      {title: "Star Wars", poster_url: "http://fallmeeting.agu.org/2015/files/2015/12/Star-Wars.jpg"},
+      {title: "Top Gun", poster_url: "http://ecx.images-amazon.com/images/I/51YimkRDEjL._SY445_.jpg"}
+    ]
+    if (this.state.hasSearched) {
+      return (
+        <Results movies={movies} />
+      )
+    } else {
+      return(
+        <Search
+          submitQuery={(e) => this.onSubmitQuery(e)}
+          searchInput={(e) => this.onSearchInput(e)}
+          query={this.state.query}
+        />
+      )
+    }
+
   }
 }
 
