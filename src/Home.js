b63be1cbc11089188b860a70b96a816a109288f4
Laura Goldstein <laura.koss.goldstein@gmail.com>
2017-03-28 08:54:14 -0400

    done

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Search from './Search';
+
+class Home extends Component {
+  render() {
+    return(
+      <div>
+      <h1>MovieOMDB</h1>
+      <Search />
+      </div>
+    )
+  }
+}
+
+export  default Home;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-// Developer TODO: Load in App component and render to the DOM
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './Home';
+import Search from './Search';
+import searchContainer from './searchContainer'
+import './index.css';
+
+ReactDOM.render(
+  <Home />,
+  document.getElementById('root')
+);
