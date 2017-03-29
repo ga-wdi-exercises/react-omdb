8c6ce5550fbaaa55b38af06236b636277a97a5e2
Dan Lin <dlin2102@gmail.com>
2017-03-28 23:53:10 -0400

    step 5

--- src/App.js
+++ src/App.js
@@ -1,1 +1,8 @@
-// Developer TODO: Define App component defintion here
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+});
