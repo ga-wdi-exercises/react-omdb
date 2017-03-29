8958d7528b9f1aff5b5698bad34fd96386a08d4f
xgironx <xgironx@gmail.com>
2017-03-28 23:30:21 -0400

    added api

--- src/SearchContainer.js
+++ src/SearchContainer.js
@@ -1,5 +1,5 @@
-                  // ####################################
-                  // 201703282333L   EL MARTES   JAY
+                               // ####################################
+                              // 201703282333L   EL MARTES   JAY
 import React, { Component } from 'react';
 import Search from './Search';
 import Results from './Results'
@@ -8,34 +8,43 @@ class SearchContainer extends Search {
                       constructor(props) {
                         super(props)
                           this.state = {
-                                          query:        ''
+                                          searchString:        ''
                                         , hasSearched:  false
                                       }
                                     }
-  onSearchInput(event) {
-                                      this.setState({ query: event.target.value })
+  onSearchStringInput(e) {
+                        this.setState({ searchString: e.target.value })
+                        console.log('SHOW ME THIS.STATE.QUERY:'   this.state.searchString);
   }
-  onSubmitQuery(event) {
-                        event.preventDefault();
-                        console.log('SHOW ME THIS.STATE.QUERY:'   this.state.query);
+  onSearchStringSubmit(e) {
+                        e.preDefault();
+                        console.log('SHOW ME THIS.STATE.SEARCHSTRING:'   this.state.searchString);
+                        // this.setState({ searchString:      '' , hasSearched: true }) }
                         this.setState({
-                                          query:      ''
-                                        , hasSearched: true
-                                      })
-                      }
+                                        searchString:      ''
+                                      , hasSearched: true
+                                    }
+                                  ,
+                                () => { console.log('SHOW ME THIS.STATE.SEARCHSTRING:'   this.state.searchString);
+                                $.ajax({
+                                    url:      `http://www.omdbapi.com/?t=${this.state.searchString}`
+                                  , type:     'get'
+                                  , dataType: 'json'
+                                }).done((response) => {
+                                  console.log('SHOW ME RESPONSE:'   response);
+                              )}
+                            })
+                    }
   render() {
     let movies = [ {title: "Star Wars", poster_url: "http://fallmeeting.agu.org/2015/files/2015/12/Star-Wars.jpg"}, {title: "Top Gun", poster_url: "http://ecx.images-amazon.com/images/I/51YimkRDEjL._SY445_.jpg"} ]
     if(this.state.hasSearched ) {
-      return (
-                <Results movies={movies} />
-              )
-    }
+      return ( <Results movies={movies} /> ) }
     else {
       return(
         <Search
-                handleSearchInput={(event) => this.onSearchInput(event)}
-                handleSubmitQuery={(event) => this.onSubmitQuery(event)}
-                query={this.state.query}
+                handleSearchInput={(e) => this.onSearchStringInput(e)}
+                handleSubmitQuery={(e) => this.onSearchStringSubmit(e)}
+                searchString={this.state.searchString}
         />
       )
     }
