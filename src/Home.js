4a3be7b8102cd3922f5a48bdc38673cc08e909a1
xgironx <xgironx@gmail.com>
2017-03-28 22:54:20 -0400

    initcommit

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import SearchContainer from './SearchContainer';
+
+class HelloWorld extends Component {
+  render() {
+    return(
+      <div>
+          <h1><a href='/'>OMDB React</a></h1>
+          <SearchContainer />
+      </div>
+    )
+  }
+}
+export default HelloWorld
---
+++ src/Search.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class Search extends Component {
+
+  render() {
+    let {handleSearchInput, handleSubmitQuery, query} = this.props
+    return(
+      <div className = 'searchBox'>
+        <form onSubmit={(event) => handleSubmitQuery(event)}>
+            <input onChange={(event) => handleSearchInput(event)}
+            value = {query}
+            type = "text"
+            placeholder= "Search a title" />
+            <button type="submit">  Search  </button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Search
