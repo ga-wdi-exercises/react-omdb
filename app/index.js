3c930b53ff8ba5766d248dd953693795da35a5d9
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2017-03-28 22:08:05 -0400

    first attempt

--- app/index.js
+++ app/index.js
@@ -1,1 +1,10 @@
-document.getElementById("app").innerHTML = "Jello World";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Main from './Main'
+
+ReactDOM.render (
+  <Main />,
+  document.getElementById('root')
+)
+
+// document.getElementById('root').appendChild(<h1>jello world</h1>)
