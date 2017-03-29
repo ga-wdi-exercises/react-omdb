45285278fc71362e3d187c5b796aa45724891cf5
Fiona <h.natapra@gmail.com>
2017-03-28 22:33:46 -0400

    updated onSubmit function to log query value

--- src/Search.js
+++ src/Search.js
@@ -2,24 +2,33 @@ import React, { Component } from 'react'
 
 
 class Search extends Component {
+  // initial state with `query` value that corresponds to a search term
   constructor() {
     super()
     this.state = {
       query: ''
     }
   }
-
+// triggered whenever the user submits the Search form
   onSubmitQuery(event) {
     event.preventDefault()
     console.log("clicked")
-
+    this.setState({
+      query: ''
+    })
+  }
+ // updates `query` value in state whenever a change is made to the input field
+  onSearchInput(e) {
+    this.setState({
+      query: e.target.value
+    })
   }
 
   render() {
     return (
       <div className="searchBox">
       <form onSubmit={(event) => this.onSubmitQuery(event)}>
-        <input type="text" placeholder="Enter Movie Title Here..." />
+        <input onChange={(event) => this.onSearchInput(event)} type="text" placeholder="Enter Movie Title Here..." />
         <button type="submit"> Search </button>
       </form>
       </div>
