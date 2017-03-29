1401ad376108798ee3db903b5dc749021eb455b4
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-03-28 22:51:58 -0400

    search form and handle events added, linked to home

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Search from './Search'
 
 class Home extends Component {
   render(){
     return(
       <div>
         <h1>Movie Finder App</h1>
+        <Search />
       </div>
     )
   }
--- src/Search.js
+++ src/Search.js
@@ -2,17 +2,30 @@ import React, { Component } from 'react';
 
 
 class Search extends Component {
+  constructor(props){
+    super()
+    this.state ={
+      query: ''
+    }
+  }
+
+  handleSearchInput(e){
+    this.setState({
+      query: e.target.value
+    })
+  }
+
+  submitQuery(e){
+    e.preventDefault()
+  }
   render(){
-    return (
-
-
-
-
-
-
-
-
-
+    return(
+      <div>
+        <form onSubmit={(e) => this.submitQuery(e)}>
+          <input onChange={(e) => this.handleSearchInput(e)} value={this.state.query} type="text" placeholder="Enter a movie title . .  ."/>
+          <button type="submit">Search</button>
+        </form>
+      </div>
 
     )
   }
