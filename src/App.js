a8b82b6ce85f250824455d1bb8eb8607aa2861dd
Fiona <h.natapra@gmail.com>
2017-03-28 21:59:05 -0400

    configured App.js & index.js, displayed HelloWorld

--- src/App.js
+++ src/App.js
@@ -1,1 +1,13 @@
-// Developer TODO: Define App component defintion here
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <h1> HelloWorld </h1>
+      </div>
+    )
+  }
+}
+
+export default App
