2f538bbbbec6c0b01c214b2f3d3c13424c0c46c7
Taylor Scott <TaylorScott@Taylors-MBP.fios-router.home>
2017-04-06 18:51:33 -0400

    done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 // Developer TODO: Load in App component and render to the DOM
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Home from './Home'
+import './index.css'
+
+ReactDOM.render(
+    <Home />,
+    document.getElementById('root')
+)
